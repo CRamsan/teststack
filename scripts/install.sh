50be7af3b216614b65151e973b537dfaa3359fba
Cesar <cesar.ramsan@gmail.com>
2013-02-03 17:17:40 -0600

    Errors fixed

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,7 @@
 
 function func_install {
 	COMMAND_INSTALL="apt-get install -y"
-	if [ "$1" -n ]
+	if [ ! -n "$1" ]
 	then
 		echo "No parameter for install function"
 		exit 1
@@ -15,13 +15,13 @@ function func_install {
 		done
 		COMMAND_INSTALL="$COMMAND_INSTALL $args"
 		echo "$COMMAND_INSTALL"
-		`$COMMAND_INSTALL`
+		$COMMAND_INSTALL
 		if [ $? -eq 0 ]
 		then
 			echo "Install process of packages $1 run correctly "
-			exit 0
 		else
 			echo "Install process of packages $1 failed"
+			exit 1
 		fi
 	fi
 }
@@ -33,8 +33,11 @@ fi
 
 ##Install NTP, set up the NTP server on your controller node so that it 
 ##receives data by modifying the ntp.conf file and restart the service.
+echo "Install NTP"
 func_install ntp
+echo "Configure NTP"
 sed -i 's/server ntp.ubuntu.com/server ntp.ubuntu.com\nserver 127.127.1.0\nfudge 127.127.1.0 stratum 10/g' /etc/ntp.conf
+echo "Restart NTP service"
 service ntp restart
 
 ##Install MySQL
@@ -42,9 +45,13 @@ service ntp restart
 ##Enter a password of your choice and verify it.
 ##Use sed to edit /etc/mysql/my.cnf to change bind-address from localhost (127.0.0.1)
 ##to any (0.0.0.0) and restart the mysql service.
+echo "Install MySQL and related packages"
 func_install python-mysqldb mysql-server
+echo "Update MySQL config"
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/my.cnf
+echo "Restart MySQL service"
 service mysql restart
 
 ##Install RabbitMQ
+echo "Install RabbitMQ"
 func_install rabbitmq-server
