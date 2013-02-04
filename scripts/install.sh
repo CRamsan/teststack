9a42e8c36c160f6c9e7eca028afa4300e8ecf008
Cesar <cesar.ramsan@gmail.com>
2013-02-03 19:31:27 -0600

    Automatic mysql installation supported

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,8 @@
 #This script is based on the tutorial that can be found at:
 #http://docs.openstack.org/trunk/openstack-compute/install/apt/content/
 
+localrc="localrc"
+
 function func_install {
 	COMMAND_INSTALL="apt-get install -y"
 	if [ ! -n "$1" ]
@@ -26,6 +28,24 @@ function func_install {
 	fi
 }
 
+function func_pre {
+	func_install debconf 
+	func_clear_values
+}
+
+function func_install_my-sql {
+	if [ ! -n "$1" ]
+	then
+        	echo "No password provided"
+		echo "Exiting now"
+           	exit 1
+       	else
+		echo mysql-server mysql-server/root_password select PASSWORD | debconf-set-selections
+		echo mysql-server mysql-server/root_password_again select PASSWORD | debconf-set-selections
+	fi
+	func_install mysql-server
+}
+
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
    exit 1
@@ -39,14 +59,45 @@ function funct_add_cloud_archive {
 	apt-get update
 }
 
+function func_get_password {
+	var=$1
+	mesg=$2
+	pw=" "
+	while true; do
+		echo "Enter a password used for: $mesg."
+		echo "If no password is provided, a random password will be generated:"
+	        read -e $var
+	        pw=${!var}
+	        [[ "$pw" = "`echo $pw | tr -cd [:alnum:]`" ]] && break
+	        echo "Invalid chars in password.  Try again:"
+	done
+	if [ ! $pw ]; then
+		pw=`openssl rand -hex 10`
+	fi
+	eval "$var=$pw"
+        echo "$var=$pw" >> $localrc
+}
+
+function func_retrieve_value {
+	key=$1
+	grep "$key" "$localrc"
+}
+
+function func_clear_values{
+	rm "$localrc"
+}
+
+func_pre
+
 ##Add the Ubuntu Cloud Archive to the repository list.
 ##This command will also update and upgrade the system.
-funct_add_cloud_archive
+#funct_add_cloud_archive
 
 ##Install NTP, set up the NTP server on your controller node so that it 
 ##receives data by modifying the ntp.conf file and restart the service.
 echo "Install NTP"
 func_install ntp
+
 echo "Configure NTP"
 sed -i 's/server ntp.ubuntu.com/server ntp.ubuntu.com\nserver 127.127.1.0\nfudge 127.127.1.0 stratum 10/g' /etc/ntp.conf
 echo "Restart NTP service"
@@ -58,7 +109,10 @@ service ntp restart
 ##Use sed to edit /etc/mysql/my.cnf to change bind-address from localhost (127.0.0.1)
 ##to any (0.0.0.0) and restart the mysql service.
 echo "Install MySQL and related packages"
-func_install python-mysqldb mysql-server
+func_install python-mysqldb
+func_get_password "ROOTPASS" "MySQL Root" 
+MYSQLPASS=$(func_retrieve_value "ROOTPASS")
+func_install_my-sql $MYSQLPASS
 echo "Update MySQL config"
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/my.cnf
 echo "Restart MySQL service"
@@ -67,3 +121,13 @@ service mysql restart
 ##Install RabbitMQ
 echo "Install RabbitMQ"
 func_install rabbitmq-server
+
+
+##Install the identity service, Keystone!
+##Install the package
+func_install keystone
+#Delete the keystone.db file created in the /var/lib/keystone directory.
+rm /var/lib/keystone/keystone.db
+
+
+#mysql -u root -p -e 
