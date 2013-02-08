46cb25384661182c9a4d70d6a04c29f5200e494f
Cesar Ramirez <cesar.ramsan@gmail.com>
2013-02-08 03:41:29 -0600

    Colored output

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ function func_install {
 	COMMAND_INSTALL="apt-get install -y"
 	if [ ! -n "$1" ]
 	then
-		echo "No parameter for install function"
+		func_echo "No parameter for install function"
 		exit 1
 	else
 		for package in "$@"
@@ -16,13 +16,13 @@ function func_install {
 			args="$args $package"
 		done
 		COMMAND_INSTALL="$COMMAND_INSTALL $args"
-		echo "$COMMAND_INSTALL"
+		func_echo "$COMMAND_INSTALL"
 		$COMMAND_INSTALL
 		if [ $? -eq 0 ]
 		then
-			echo "Install process of packages $1 run correctly "
+			func_echo "Install process of packages $1 run correctly "
 		else
-			echo "Install process of packages $1 failed"
+			func_echo "Install process of packages $1 failed"
 			exit 1
 		fi
 	fi
@@ -35,8 +35,8 @@ function func_pre {
 function func_install_my-sql {
 	if [ ! -n "$1" ]
 	then
-        	echo "No password provided"
-		echo "Exiting now"
+        	func_echo "No password provided"
+		func_echo "Exiting now"
            	exit 1
        	else
 		PASSWORD=$1
@@ -47,8 +47,8 @@ function func_install_my-sql {
 }
 
 function funct_add_cloud_archive {
-	apt-get update
-	apt-get upgrade -y
+#	apt-get update
+#	apt-get upgrade -y
 	apt-get install ubuntu-cloud-keyring
 	echo "deb http://ubuntu-cloud.archive.canonical.com/ubuntu precise-updates/folsom main" > /etc/apt/source.d/folsom.list
 	apt-get update
@@ -71,16 +71,16 @@ function func_set_password {
 	mesg=$2
 	pw=" "
 	while true; do
-		echo "Enter a password used for: $mesg."
-		echo "If no password is provided, a random password will be generated:"
+		func_echo "Enter a password used for: $mesg."
+		func_echo "If no password is provided, a random password will be generated:"
 	        read -e $var
 	        pw=${!var}
 	        [[ "$pw" = "`echo $pw | tr -cd [:alnum:]`" ]] && break
-	        echo "Invalid chars in password.  Try again:"
+	        func_echo "Invalid chars in password.  Try again:"
 	done
 	if [ ! $pw ]; then
 		pw=`openssl rand -hex 10`
-		echo "Password: $pw"
+		func_echo "Password: $pw"
 	fi
 	func_set_value "$var" "$pw"
 }
@@ -99,7 +99,7 @@ function func_replace_param {
 	parameter=$2
 	newvalue=$3
 
-	echo "In file $file - Parameter \"$parameter\" is been set to \"$newvalue\""
+	func_echo "In file $file - Parameter \"$parameter\" is been set to \"$newvalue\""
 
 	oldline=$(sed 's/ //g' $file | grep "^$parameter=")
 	sed -i 's/ //g' $file
@@ -108,9 +108,9 @@ function func_replace_param {
 	sed -i "s/$oldlinefixed/$parameter=$newvaluefixed/g" $file
 	newline=$(cat $file | grep "^$parameter=")
 
-	echo $oldline
-	echo "V-V-V-V-V-V-V-V"
-	echo $newline
+	func_echo $oldline
+	func_echo "V-V-V-V-V-V-V-V"
+	func_echo $newline
 }
 
 function func_create_tenant {
@@ -149,27 +149,35 @@ function func_user_role_add {
 	keystone --token "$ADMINTOKEN" --endpoint http://"$KEYSTONEIP":35357/v2.0 user-role-add --user-id "$USERID" --tenant-id "$TENANTID" --role-id "$ROLEID"
 }
 
+function func_echo {
+	MSG=$1
+	echo -e "\E[32m$MSG"
+	tput sgr0
+}
+
 ###################################################################################
 
+##Check for admin rights
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
 
-func_pre
+##Run all the prerequisites
+#func_pre
 
 ##Add the Ubuntu Cloud Archive to the repository list.
 ##This command will also update and upgrade the system.
-funct_add_cloud_archive
+#funct_add_cloud_archive
 
 ##Install NTP, set up the NTP server on your controller node so that it 
 ##receives data by modifying the ntp.conf file and restart the service.
-echo "Install NTP"
+func_echo "Install NTP"
 func_install ntp
 
-echo "Configure NTP"
+func_echo "Configure NTP"
 sed -i 's/server ntp.ubuntu.com/server ntp.ubuntu.com\nserver 127.127.1.0\nfudge 127.127.1.0 stratum 10/g' /etc/ntp.conf
-echo "Restart NTP service"
+func_echo "Restart NTP service"
 service ntp restart
 
 ##Install MySQL
@@ -177,7 +185,7 @@ service ntp restart
 ##Enter a password of your choice and verify it.
 ##Use sed to edit /etc/mysql/my.cnf to change bind-address from localhost (127.0.0.1)
 ##to any (0.0.0.0) and restart the mysql service.
-echo "Install MySQL and related packages"
+func_echo "Install MySQL and related packages"
 func_install python-mysqldb
 
 if [ ! -n "$MYSQLPASS" ]
@@ -187,13 +195,22 @@ then
 fi
 func_install_my-sql $MYSQLPASS
 
-echo "Update MySQL config"
+func_echo "Update MySQL config"
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/my.cnf
-echo "Restart MySQL service" func_create_user
+func_echo "Restart MySQL service" func_create_user
 service mysql restart
 
+TMP=$(mysql -u root -p"$MYSQLPASS" -e "quit")
+if [ -n "$TMP" ]
+then
+	func_echo "Error accessing database."
+	exit
+else
+	func_echo "Database access successful"
+fi
+
 ##Install RabbitMQ
-echo "Install RabbitMQ"
+fund_echo "Install RabbitMQ"
 func_install rabbitmq-server
 
 ###################################################################################
@@ -230,6 +247,11 @@ fi
 
 ##Configure Keystone to use mysql.
 func_replace_param "/etc/keystone/keystone.conf" "connection" "mysql://keystone:$KEYSTONEPASS@$KEYSTONEIP/keystone"
+
+##Next, restart the keystone service so that it picks up the new database configuration.
+##Lastly, initialize the new keystone database.
+service keystone restart
+keystone-manage db_sync
 
 ##Check for the existance of an AdminToken.
 if [ ! -n "$ADMINTOKEN" ]
