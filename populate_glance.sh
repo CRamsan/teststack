c6286adb589073603407c394b97da6b59dd5b815
cesar <cesar@ubuntu.(none)>
2013-03-11 19:02:37 -0500

    New script for populating glance

--- populate_glance.sh
+++ populate_glance.sh
@@ -15,4 +15,20 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-glance image-create --location http://uec-images.ubuntu.com/releases/12.04/release/ubuntu-12.04-server-cloudimg-amd64-disk1.img --is-public true --disk-format qcow2 --container-format bare --name "Ubuntu"
+#DEFTENANTNAME=admin
+#export DEFTENANTID=b2f1e5a7ef574673b91d84fdd073eff0
+#export ADMINUSERNAME=admin
+#export ADMINUSERPASS=d8bce88e6446532b0b37
+#export ADMINUSERID=265bb4704f5a4585ae157322842ff5c0
+#export ADMINROLENAME=admin
+
+glance	--os-username="$ADMINUSERNAME" \
+	--os-password="$ADMINUSERPASS" \
+	--os-tenant-id="$DEFTENANTID" \
+	--os-auth-url="http://$GLANCEIP:5000/v2.0/" \
+ 	image-create \
+	--location http://uec-images.ubuntu.com/releases/12.04/release/ubuntu-12.04-server-cloudimg-amd64-disk1.img \
+	--is-public true \
+	--disk-format qcow2 \
+	--container-format bare \
+	--name "Ubuntu"
