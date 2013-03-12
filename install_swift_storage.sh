29aebfc5a81bb405bb40d9aa11d71539bdea35e0
cesar <cesar@ubuntu.(none)>
2013-03-12 03:47:40 -0500

    New file added

---
+++ install_swift_storage.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#This script is based on the tutorial that can be found at:
+#http://docs.openstack.org/trunk/openstack-compute/install/apt/content/
+
+localrc="localrc"
+
+source functions.sh
+source $localrc
+
+###################################################################################
+
+##Check for admin rights
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+func_install swift-account swift-container swift-object xfsprogs
+
+exit 
+mkdir -p /etc/swift
+chown -R swift:swift /etc/swift/
+
+if [ ! -n "$SWIFTHASH" ]
+then
+        func_set_password "SWIFTHASH" "Swift Hash"
+        SWIFTHASH=$(func_retrieve_value "SWIFTHASH")
+fi
+
+echo "[swift-hash]" > /etc/swift/swift.conf
+echo "# random unique string that can never change (DO NOT LOSE)" >> /etc/swift/swift.conf
+echo "swift_hash_path_suffix = $SWIFTHASH" >> /etc/swift/swift.conf
+
+
