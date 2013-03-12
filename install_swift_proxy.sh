d673c235b4b30b6b3562e97c91609ae7366befcb
cesar <cesar@ubuntu.(none)>
2013-03-12 13:02:27 -0500

    Swift proxy installation splitted in two

--- install_swift_proxy.sh
+++ install_swift_proxy.sh
@@ -75,23 +75,3 @@ cd /etc/swift
 swift-ring-builder account.builder create 5 3 1
 swift-ring-builder container.builder create 5 3 1
 swift-ring-builder object.builder create 5 3 1
-
-
-echo "Give the IP of a storage node"
-NODEIP=$(func_ask_user)
-func_set_value "NODEIP" $NODEIP
-
-swift-ring-builder account.builder add z1-$NODEIP:6002/sdb1 100
-swift-ring-builder container.builder add z1-$NODEIP:6001/sdb1 100
-swift-ring-builder object.builder add z1-$NODEIP:6000/sdb1 100
-
-swift-ring-builder account.builder
-swift-ring-builder container.builder
-swift-ring-builder object.builder
-
-swift-ring-builder account.builder rebalance
-swift-ring-builder container.builder rebalance
-swift-ring-builder object.builder rebalance
-
-chown -R swift:swift /etc/swift
-swift-init proxy start
