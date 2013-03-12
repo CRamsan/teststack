6611f26ee9bf64b87f3282dc979c1f1318033214
cesar <cesar@ubuntu.(none)>
2013-03-12 03:31:00 -0500

    New install script for swift

---
+++ install_swift_base.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#This script is based on the tutorial that can be found at:
+#http://docs.openstack.org/trunk/openstack-compute/install/apt/content/
+
+localrc="localrc"
+
+source functions.sh
+source $localrc
+
+###################################################################################
+
+##Check for admin rights
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+func_install swift rsync memcached python-netifaces python-xattr python-memcache
+
+mkdir -p /etc/swift
+chown -R swift:swift /etc/swift/
+
